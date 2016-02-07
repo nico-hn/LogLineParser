69eafd599451aa8db737783fecbfd6eb70ab5586
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2016-02-07 23:08:49 +0900

    added a test for EscapeNode#remove_escaped_part

--- test/test_log_line_parser.rb
+++ test/test_log_line_parser.rb
@@ -38,4 +38,14 @@ class TestLogLineParser < Minitest::Test
     re = LogLineParser::StringEscapeNode.to_be_escaped_re
     assert_equal(/\A(?:\\|t|n|r)/, re)
   end
+
+  def test_escape_node_remove_escaped_part
+    escape_node = LogLineParser::StringEscapeNode.new
+    string_with_a_tab = "string that begins with a tab"
+    string_with_a_double_quote = "string with a double quote"
+    assert_equal(string_with_a_tab,
+                 escape_node.remove_escaped_part("t" + string_with_a_tab))
+    assert_equal(string_with_a_double_quote,
+                 escape_node.remove_escaped_part('"' + string_with_a_double_quote))
+  end
 end
