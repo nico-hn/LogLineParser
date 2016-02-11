f35a006e63c81fb168a8460f5619ee7e161368db
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2016-02-11 14:41:36 +0900

    added an assertion to see if a domain name only referer can be parsed properly.

--- test/test_log_line_parser.rb
+++ test/test_log_line_parser.rb
@@ -3,6 +3,7 @@ require 'minitest_helper'
 class TestLogLineParser < Minitest::Test
   def setup
     @log_line = '192.168.3.4 - quidam [07/Feb/2016:07:39:42 +0900] "GET /index.html HTTP/1.1" 200 432 "http://www.example.org/start.html" "Mozilla/5.0 (X11; U; Linux i686; ja-JP; rv:1.7.5) Gecko/20041108 Firefox/1.0"'
+    @log_line2 = '192.168.3.4 - quidam [07/Feb/2016:07:39:42 +0900] "GET /index.html HTTP/1.1" 200 432 "http://www.example.org/" "Mozilla/5.0 (X11; U; Linux i686; ja-JP; rv:1.7.5) Gecko/20041108 Firefox/1.0"'
   end
 
   def test_that_it_has_a_version_number
@@ -84,6 +85,7 @@ class TestLogLineParser < Minitest::Test
 
   def test_combined_log_record
     record = LogLineParser.parse(@log_line).to_record
+    record2 = LogLineParser.parse(@log_line2).to_record
     expected_user_agent = 'Mozilla/5.0 (X11; U; Linux i686; ja-JP; rv:1.7.5) Gecko/20041108 Firefox/1.0'
     expected_last_request_status = 200
     expected_time = "2016-02-07 07:39:42 +0900"
@@ -93,6 +95,7 @@ class TestLogLineParser < Minitest::Test
     assert_equal("GET", record.method)
     assert_equal("http://www.example.org/", record.referer_url)
     assert_equal("/start.html", record.referer_resource)
+    assert_equal("/", record2.referer_resource)
   end
 
   def test_combined_log_record_date
