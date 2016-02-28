2e904ff60e607c881f43c40475cfcdda72e01069
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2016-02-28 11:16:30 +0900

    added a test for Query#referred_from_resources?

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'log_line_parser'
 require 'log_line_parser/utils'
+require 'log_line_parser/query'
 
 require 'minitest/autorun'
---
+++ test/test_log_line_parser_query.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+require 'minitest_helper'
+
+
+class TestLogLineParserQuery < Minitest::Test
+  include LogLineParser
+
+  def setup
+    @log_line = '192.168.3.4 - quidam [07/Feb/2016:07:39:42 +0900] "GET /index.html HTTP/1.1" 200 432 "http://www.example.org/start.html" "Mozilla/5.0 (X11; U; Linux i686; ja-JP; rv:1.7.5) Gecko/20041108 Firefox/1.0"'
+    @log_line2 = '192.168.3.4 - quidam [07/Feb/2016:07:39:42 +0900] "GET /index.html HTTP/1.1" 200 432 "http://www.example.org/" "Mozilla/5.0 (X11; U; Linux i686; ja-JP; rv:1.7.5) Gecko/20041108 Firefox/1.0"'
+    @log_line3 = '192.168.3.4 - quidam [07/Feb/2016:07:39:42 +0900] "GET /subdir/index.html HTTP/1.1" 200 432 "http://www.example.org" "Mozilla/5.0 (X11; U; Linux i686; ja-JP; rv:1.7.5) Gecko/20041108 Firefox/1.0"'
+    @log_line4 = '192.168.3.4 - quidam [07/Feb/2016:07:39:42 +0900] "GET /subdir/index.html HTTP/1.1" 200 432 "http://www.example.org/subdir/" "Mozilla/5.0 (X11; U; Linux i686; ja-JP; rv:1.7.5) Gecko/20041108 Firefox/1.0"'
+  end
+
+  def test_referred_from_resources?
+    record = CombinedLogRecord.parse(@log_line)
+    record2 = CombinedLogRecord.parse(@log_line2)
+    query = Query.new(domain: "www.example.org", resources: ["/start.html"])
+    query_without_domain = Query.new(resources: ["/start.html"])
+    assert_equal(true, query.referred_from_resources?(record))
+    assert_equal(true, query_without_domain.referred_from_resources?(record))
+    assert_equal(false, query.referred_from_resources?(record2))
+  end
+end
