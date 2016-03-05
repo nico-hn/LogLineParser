e09c5bca48f4c19f7bd1f1ef3bf1491d4fa3202e
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2016-03-06 01:15:33 +0900

    renamed MoeLogRecord MoeLogParser

--- lib/log_line_parser/moe.rb
+++ lib/log_line_parser/moe.rb
@@ -6,7 +6,7 @@ require 'log_line_parser/utils'
 module LogLineParser
   # CombinedLogFormat + "%D"
   MoeLogFormat = "%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-agent}i\" %D"
-  MoeLogRecord = parser(MoeLogFormat)
-  PREDEFINED_FORMATS['moe'] = MoeLogRecord
+  MoeLogParser = parser(MoeLogFormat)
+  PREDEFINED_FORMATS['moe'] = MoeLogParser
 end
 
