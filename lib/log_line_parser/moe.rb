b14521575c1c7dcc37b4bb01f0c61870fb1ec09b
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2016-03-07 01:07:16 +0900

    You can now choose MoeLogParser from the command-line application "log_line_parser"

--- lib/log_line_parser/moe.rb
+++ lib/log_line_parser/moe.rb
@@ -3,6 +3,12 @@
 require 'log_line_parser'
 require 'log_line_parser/utils'
 
+# MoeLogParser is added from the personal needs of the original author,
+# and the LogFormat for it is not a widely used format.
+# You may remove this file if you don't need it.
+# (MOE is the acronym of the organization's name for which the author
+# is working at the time of the first release of this program.)
+
 module LogLineParser
   # CombinedLogFormat + "%D"
   MoeLogFormat = "%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-agent}i\" %D"
