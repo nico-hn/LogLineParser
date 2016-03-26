8eff1e6a32cb1e518313817b4efb9f34ec7cd2e8
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2016-03-26 23:54:34 +0900

    The comment about MoeLogParser should be placed above its definition.

--- lib/log_line_parser/moe.rb
+++ lib/log_line_parser/moe.rb
@@ -3,13 +3,14 @@
 require 'log_line_parser'
 require 'log_line_parser/utils'
 
-# MoeLogParser is added from the personal needs of the original author,
-# and the LogFormat for it is not a widely used format.
-# You may remove this file if you don't need it.
-# (MOE is the acronym of the organization's name for which the author
-# is working at the time of the first release of this program.)
-
 module LogLineParser
+
+  # MoeLogParser is added from the personal needs of the original author,
+  # and the LogFormat for it is not a widely used one.
+  # You may remove this file if you don't need it.
+  # (MOE is the acronym of the name of the organization for which
+  # the author is working at the time of the first release of this program.)
+  #
   # CombinedLogFormat + "%D"
   MoeLogFormat = "%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-agent}i\" %D"
   MoeLogParser = parser(MoeLogFormat)
