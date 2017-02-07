bc8157a5c6974d93c5cfd36325eba09017fd9590
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2017-02-07 13:57:10 +0900

    refactoring of Tokenizer.tokenize:
    The previous version does not take into account non-ascii charactors

--- lib/log_line_parser/line_parser.rb
+++ lib/log_line_parser/line_parser.rb
@@ -7,11 +7,11 @@ module LineParser
 
       def tokenize(str, tokens=[])
         @scanner.string = str
-        cur_pos = @scanner.charpos
+        cur_pos = 0 # in stead of @scanner.pos
         while chunk_length = @scanner.skip_until(@special_token_re)
           token = @scanner.matched
-          non_token_length = chunk_length - token.length
-          tokens.push str[cur_pos, non_token_length] if non_token_length > 0
+          non_token_length = chunk_length - token.bytesize
+          tokens.push str.byteslice(cur_pos, non_token_length) if non_token_length > 0
           tokens.push token
           cur_pos += chunk_length
         end
